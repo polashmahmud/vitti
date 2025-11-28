ac5ef01bb91bc9bc0e917273135a0110c14cfcf4
Polash Mahmud <polashmahmud@gmail.com>
2025-11-28 15:59:51 +0600

    feat: add Sheet and Sidebar components with various subcomponents
    - Introduced SheetOverlay, SheetTitle, and SheetTrigger components for managing overlay sheets.
    - Implemented Sidebar component along with SidebarContent, SidebarFooter, SidebarGroup, SidebarMenu, and related subcomponents for enhanced sidebar functionality.
    - Added utility functions for managing sidebar state and responsiveness.
    - Created Skeleton and Tooltip components for improved UI feedback and interactions.

--- resources/js/components/common/AppSidebar.vue
+++ resources/js/components/common/AppSidebar.vue
@@ -1,8 +1,54 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
+import { Calendar, Home, Inbox, Search } from 'lucide-vue-next';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarInset, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarRail, SidebarTrigger, } from '@/components/ui/sidebar'
+
+
+const items = [
+    {
+        title: 'Home',
+        href: '/',
+        icon: Home
+    },
+    {
+        title: 'Inbox',
+        href: '#',
+        icon: Inbox
+    },
+    {
+        title: 'Calendar',
+        href: '#',
+        icon: Calendar
+    },
+    {
+        title: 'Search',
+        href: '#',
+        icon: Search
+    }
+];
 </script>
 
 <template>
-    <div>
-        Sidebar
-    </div>
+    <Sidebar>
+        <SidebarHeader>
+        </SidebarHeader>
+        <SidebarContent>
+            <SidebarGroup>
+                <SidebarGroupLabel>Application</SidebarGroupLabel>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem v-for="item in items" :key="item.title">
+                            <SidebarMenuButton as-child>
+                                <Link :href="item.href">
+                                <component :is="item.icon" />
+                                <span>{{ item.title }}</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter></SidebarFooter>
+    </Sidebar>
 </template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/common/AppSidebar.vue';
 import Navbar from '@/components/common/Navbar.vue';
+import SidebarProvider from '@/components/ui/sidebar/SidebarProvider.vue';
 
 </script>
 
 <template>
     <div class="flex">
-        <AppSidebar />
-        <main class="w-full">
-            <Navbar />
-            <div class="px-4">
-                <slot />
-            </div>
-        </main>
+        <SidebarProvider>
+            <AppSidebar />
+            <main class="w-full">
+                <Navbar />
+                <div class="px-4">
+                    <slot />
+                </div>
+            </main>
+        </SidebarProvider>
     </div>
 </template>
