5ddee094796d1b5183d7a512564928625ecb2968
Polash Mahmud <polashmahmud@gmail.com>
2025-11-28 14:46:53 +0600

    laravel setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+
+    <Head title="Home" />
+    <div class="p-4">
+        <Button variant="destructive">Hello Polash</Button>
+    </div>
+</template>
