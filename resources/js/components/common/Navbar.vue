419e7861bea19abff1814b9484068e336c73bb31
Polash Mahmud <polashmahmud@gmail.com>
2025-11-28 15:40:33 +0600

    feat: add dropdown menu components and update dependencies

--- resources/js/components/common/Navbar.vue
+++ resources/js/components/common/Navbar.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import { Moon } from 'lucide-vue-next';
+import { LogOut, Moon, Settings, User } from 'lucide-vue-next';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger, } from '@/components/ui/dropdown-menu'
 </script>
 
 <template>
@@ -15,10 +16,30 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
         <div class="flex items-center gap-4">
             <Link href="/">Home</Link>
             <Moon />
-            <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent :side-offset="10">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>
+                        <User class="size-[1.2rem]" />
+                        Profile
+                    </DropdownMenuItem>
+                    <DropdownMenuItem>
+                        <Settings class="size-[1.2rem]" />
+                        Settings
+                    </DropdownMenuItem>
+                    <DropdownMenuItem variant="destructive">
+                        <LogOut class="size-[1.2rem]" />
+                        Logout
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </div>
     </nav>
 </template>
